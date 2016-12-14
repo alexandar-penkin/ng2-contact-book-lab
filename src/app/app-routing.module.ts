a30384fcd130b5f00ebb04a73dc78c51339277bb
Alexandar Penkin <lex@esof.net>
2016-12-14 12:49:01 +0200

    Create ContactList and ContactDetail modules, components and templates; Create primitive routing; works!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }     from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { ContactListComponent }  from './contacts/contact-list.component';
+import { ContactDetailComponent }  from './contacts/contact-detail.component';
+
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      // { path: '', redirectTo: '/home', pathMatch: 'full' },
-      // { path: 'home', component: HomeComponent },
-      // { path: 'products', component: ProductListComponent },
-      // { path: 'users', component: UserListComponent }
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: ContactListComponent },
+      { path: 'details', component: ContactDetailComponent }
     ])
   ],
   exports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule }     from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppNavComponent } from './app-nav.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactDetailModule }  from './contacts/contact-detail.module';
+import { ContactListModule }  from './contacts/contact-list.module';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule, ContactListModule, ContactDetailModule],
   declarations: [AppComponent, AppNavComponent],
   exports: [AppComponent],
   bootstrap: [ AppComponent ]
